87ec60b99477439de0a01354fa844be5f8b8dd3f
Romain <romain.richard.19@gmail.com>
2022-04-16 20:42:09 +0200

    :construction: design shopping list

--- src/components/ItemShoppingList/index.js
+++ src/components/ItemShoppingList/index.js
@@ -1,7 +1,7 @@
 function ItemShoppingList(props) {
     const {item} = props;
     return (
-        <div className="flex items-stretch">
+        <div className="flex items-stretch px-4 py-2">
             <div>{item.content}</div>
         </div>
     );
